5ccb5cae4de419e70fcafb22f48baed22e6cf6c1
bchutchison <benhutchison85@gmail.com>
2019-09-19 15:01:09 +0100

    case insensitive ilike added

--- src/main/java/com/codeclan/bookingsystem/bookingsystem/projections/EmbedCourseAndCustomer.java
+++ src/main/java/com/codeclan/bookingsystem/bookingsystem/projections/EmbedCourseAndCustomer.java
@@ -5,11 +5,9 @@ import com.codeclan.bookingsystem.bookingsystem.models.Course;
 import com.codeclan.bookingsystem.bookingsystem.models.Customer;
 import org.springframework.data.rest.core.config.Projection;
 
-
     @Projection(name="embedCourseAndCustomer", types = Booking.class)
     public interface EmbedCourseAndCustomer{
        public String getDate();
        public Course getCourse();
        public Customer getCustomer();
-
     }
