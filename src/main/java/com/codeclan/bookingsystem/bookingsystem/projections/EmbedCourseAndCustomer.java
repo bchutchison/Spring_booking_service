6b7c728f0539db8d79dea4d62d47b71963f441e0
bchutchison <benhutchison85@gmail.com>
2019-09-19 14:55:34 +0100

    error correction

--- src/main/java/com/codeclan/bookingsystem/bookingsystem/projections/EmbedCourseAndCustomer.java
+++ src/main/java/com/codeclan/bookingsystem/bookingsystem/projections/EmbedCourseAndCustomer.java
@@ -5,13 +5,11 @@ import com.codeclan.bookingsystem.bookingsystem.models.Course;
 import com.codeclan.bookingsystem.bookingsystem.models.Customer;
 import org.springframework.data.rest.core.config.Projection;
 
-public interface EmbedCourseAndCustomer {
 
     @Projection(name="embedCourseAndCustomer", types = Booking.class)
-    public interface EmberCourseAndCustomer{
+    public interface EmbedCourseAndCustomer{
        public String getDate();
        public Course getCourse();
        public Customer getCustomer();
 
     }
-}
